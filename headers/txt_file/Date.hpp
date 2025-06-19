ea13416bb8e6adaee8131e01ef3cc6080d869282
chhoflac <chhoflac@student.42.fr>
2025-06-19 10:28:03 +0200

    adding files

---
+++ headers/txt_file/Date.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Date.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 16:02:44 by chhoflac          #+#    #+#             */
+/*   Updated: 2025/06/18 17:22:59 by chhoflac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <iostream>
+
+class Date{
+    private :
+        std::string dateString;
+        bool        isLeapYear;
+        int         day;
+        int         month;
+        int         year;
+        bool        isValid;
+    public:
+        Date(); //random day, month and year (valid or not)
+        Date(bool isValid); // deciding if it will be valid or not and create random date
+        Date(int day, int month, int year); // choose directly the date (check date has to be done)
+        Date(const Date &src); //copy constructor
+        ~Date(); //destructor;
+        
+        Date &operator = (const Date &src);
+
+        std::string         getDate() const;
+        int                 getDay() const;
+        int                 getMonth() const;
+        int                 getYear() const;
+        bool                getIsValid() const;
+        bool                getIsLeap() const;
+
+        std::string         transformToDateString() const; //parse values into std string
+
+        void                dateGenerator(bool isValid); //creates a random date
+        
+        bool                validDefiner() const; //define wether the date is valid or not
+        bool                isLeap();
+};
+
+std::ostream &operator<<(std::ostream &os, const Date &date);
+
+
