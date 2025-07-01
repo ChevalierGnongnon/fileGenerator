9ebd6d79bf514663512f7822dca1b0ed252f627e
chhoflac <chhoflac@student.42.fr>
2025-07-01 14:40:56 +0200

    separating projects

--- general_files/includes/Interfaces/ILine.hpp
+++ general_files/includes/Interfaces/ILine.hpp
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ALine.hpp                                          :+:      :+:    :+:   */
+/*   ILine.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 13:03:35 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/06/19 13:05:31 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/07/01 13:48:13 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
-
-class ALine{
-	private :
+class ILine{
+	public:
+		virtual ~ILine();
 		
-		
+		virtual void	printOnFile() const = 0;
+		virtual ILine	*duplicate() const = 0;	
 };
